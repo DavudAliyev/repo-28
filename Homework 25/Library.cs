6a7465342bca852981c79f339f94800f259662b6
Davud <davud.aliyev2007@gmail.com>
2024-02-01 15:49:06 +0400

    Add project files.

---
+++ Homework 25/Library.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_25
+{
+    internal class Library
+    {
+        public Library[] _books=new Library[0];
+        public Library[] Books => _books;
+        public int BookLimit;
+        public int No;
+
+
+        public void AddBook(Library books)
+        {
+            Array.Resize(ref _books, _books.Length+1);
+            _books[_books.Length-1] = books;
+
+            for(int i=0; i<_books.Length; i++)
+            {
+                if(books is books[i])
+                {
+
+                }
+            }
+        }
+
+        public Library FindBookByNo(int no)
+        {
+            for(int i=0; i<_books.Length; i++)
+            {
+                return _books[i];
+            }
+            return null;
+        }
+
+        public int FindIndexByNo(int no)
+        {
+            for (int i = 0; i < _books.Length; i++)
+            {
+                if (_books[i].No == no)
+                {
+                    return i;
+                }
+                
+            }
+            return -1;
+        }
+
+        public bool RemoveBook(int no)
+        {
+            var wantedbook = FindBookByNo(no);
+
+            if (wantedbook == null) return false;
+
+            var wantedIndex=FindIndexByNo(no);
+
+            for(int i=wantedIndex;i<_books.Length;i++)
+            {
+                var temp = _books[i];
+                _books[i] = _books[i+1];
+                _books[i+1] = temp;
+            }
+            Array.Resize(ref _books, _books.Length-1);
+            return true;
+        }
+    }
+}
---
+++ Homework 25/Program.cs
@@ -0,0 +1,48 @@
+﻿using Homework_25;
+using System.Collections.Concurrent;
+
+Dictionary<string, DateTime> exam=new Dictionary<string, DateTime>();
+exam.Add("riyaziyyat", new DateTime(2010, 12, 23));
+
+foreach(KeyValuePair<string, DateTime> item in exam)
+{
+    Console.WriteLine(item.Key);
+    Console.WriteLine(item.Value);
+}
+
+
+string opt;
+
+do
+{
+    Console.WriteLine("1. Add book");
+    Console.WriteLine("2. Delete book");
+    Console.WriteLine("3. Show all books");
+    Console.WriteLine("4. Search book");
+    Console.WriteLine("0. Cix");
+
+    Console.WriteLine("Emeliyyati secin: ");
+    opt= Console.ReadLine();
+
+    switch (opt)
+    {
+        case "1":
+            Library
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+
+    }
+
+} while (opt != "0");
+
+void AddBook()
+{
+    Console.WriteLine("Book name: ");
+    string name=Console.ReadLine();
+    
+}
